89412420318d80ebbec08bde57f5079bed90fb89
Roben Kleene <robenk@gmail.com>
2014-09-01 12:09:33 -0400

    Answer 1.4

--- 1.1 Elements of Programming.rkt
+++ 1.1 Elements of Programming.rkt
@@ -47,3 +47,18 @@
 	(else (sum-of-squares x y))))
 
 (sum-of-squares-of-two-larger-numbers 3 4 5) ;; 41
+
+;; Exercise 1.4
+
+;; Observe that our model of evaluation allows for combinations whose operators are compound expressions. Use this observation to describe the behavior of the following procedure:
+
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+(a-plus-abs-b 5 5) ;; 10
+(a-plus-abs-b 5 -5) ;; 10
+(a-plus-abs-b -5 -5) ;; 0
+
+;; The if statement returns the `+` operator if `b` is greater than zero otherwise the `-` operator. The returned operator is then used to evaluate the arguements: `a` and `b`. In this way, the absolute value of `b` is added to the value of `a`.
+
+
