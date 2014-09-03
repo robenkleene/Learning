87336db203d8c351b8402af781ea42d61aeaab00
Roben Kleene <robenk@gmail.com>
2014-09-02 20:04:33 -0400

    Add square root

--- 1.1 Elements of Programming.scm
+++ 1.1 Elements of Programming.scm
@@ -79,3 +79,19 @@
 ;; Exercise 1.6
 
 ;; An infinite loop because all three of the arguments to `new-if` will be evaluated before the body of `new-if` even executes. Since the third argument to `new-if` calls the `sqrt-iter` procedure itself again, this will result an infinite recursive calls to `sqrt-ter`.
+
+(define (sqrt x)
+  (define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+	guess
+	(sqrt-iter (improve guess x)
+		   x)))
+  (define (improve guess x)
+    (average guess (/ x guess)))
+  (define (average x y)
+    (/ (+ x y) 2))
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.001))
+  (sqrt-iter 1.0 x))
+
+(sqrt 9)
