da571d654ad65bbf9297540b4a04c137759c772e
Roben Kleene <robenk@gmail.com>
2014-09-03 18:52:47 -0400

    Use Emacs Lisp comment style

--- 1.1 Elements of Programming.scm
+++ 1.1 Elements of Programming.scm
@@ -1,22 +1,22 @@
-;; Exercise 1.1
+;;;; Exercise 1.1
 
-(define a 3) ;; 3
-(define b (+ a 1)) ;; void
-(+ a b (* a b)) ;; 19
-(= a b) ;; #f
+(define a 3)				; 3
+(define b (+ a 1))			; void
+(+ a b (* a b))				; 19
+(= a b)					; #f
 (if (and (> b a) (< b (* a b)))
     b
-    a) ;; 4
+    a)					; 4
 (cond ((= a 4) 6)
       ((= b 4) (+ 6 7 a))
-      (else 25)) ;; 16
-(+ 2 (if (> b a) b a)) ;; 6
+      (else 25))			; 16
+(+ 2 (if (> b a) b a))			; 6
 (* (cond ((> a b) a)
          ((< a b) b)
          (else -1))
-   (+ a 1)) ;; 16
+   (+ a 1))				; 16
 
-;; Exercise 1.2
+;;;; Exercise 1.2
 
 (/ (+ (+ 5 4)
       (- 2
@@ -25,11 +25,11 @@
 	       (/ 4 5)))))
    (* 3
       (* (- 6 2)
-	 (- 2 7)))) ;; -37/150
+	 (- 2 7))))			; -37/150
 
-;; Exercise 1.3
+;;;; Exercise 1.3
 
-;; Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
+;;; Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers.
 
 
 (define (sum-of-squares-of-two-larger-numbers x y z)
@@ -45,11 +45,11 @@
 	((= (smallest x y z) y) (sum-of-squares x z))
 	(else (sum-of-squares x y))))
 
-(sum-of-squares-of-two-larger-numbers 3 4 5) ;; 41
+(sum-of-squares-of-two-larger-numbers 3 4 5) ; 41
 
-;; Exercise 1.4
+;;;; Exercise 1.4
 
-;; Observe that our model of evaluation allows for combinations whose operators are compound expressions. Use this observation to describe the behavior of the following procedure:
+;;;; Observe that our model of evaluation allows for combinations whose operators are compound expressions. Use this observation to describe the behavior of the following procedure:
 
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
@@ -58,26 +58,26 @@
 (a-plus-abs-b 5 -5) ;; 10
 (a-plus-abs-b -5 -5) ;; 0
 
-;; The if statement returns the `+` operator if `b` is greater than zero otherwise the `-` operator. The returned operator is then used to evaluate the arguements: `a` and `b`. In this way, the absolute value of `b` is added to the value of `a`.
+;;; The if statement returns the `+` operator if `b` is greater than zero otherwise the `-` operator. The returned operator is then used to evaluate the arguements: `a` and `b`. In this way, the absolute value of `b` is added to the value of `a`.
 
-;; Exercise 1.5
+;;;; Exercise 1.5
 
-;; (define (p) (p))
+;;; (define (p) (p))
 
 (define (test x y)
   (if (= x 0)
       0
       y))
 
-;; (test 0 (p))
+;;; (test 0 (p))
 
-;; Applicative-order evaluation will result in an infinite loop, because in applicative-order evaluation arguments are evaluated before they are applied. So p calls itself before the body of the `test` procedure is entered.
+;;; Applicative-order evaluation will result in an infinite loop, because in applicative-order evaluation arguments are evaluated before they are applied. So p calls itself before the body of the `test` procedure is entered.
 
 ;; Normal-order evaluation will not result in an infinite loop, because in normal-order evaluation arguments are not evaluated until they are needed. So Ben's call to the `test`.
 
-;; Exercise 1.6
+;;;; Exercise 1.6
 
-;; An infinite loop because all three of the arguments to `new-if` will be evaluated before the body of `new-if` even executes. Since the third argument to `new-if` calls the `sqrt-iter` procedure itself again, this will result an infinite recursive calls to `sqrt-ter`.
+;;; An infinite loop because all three of the arguments to `new-if` will be evaluated before the body of `new-if` even executes. Since the third argument to `new-if` calls the `sqrt-iter` procedure itself again, this will result an infinite recursive calls to `sqrt-ter`.
 
 (define (sqrt x)
   (define (sqrt-iter guess x)
@@ -93,4 +93,7 @@
     (< (abs (- (square guess) x)) 0.001))
   (sqrt-iter 1.0 x))
 
-(sqrt 9)
+(sqrt 9)				; 3.00009155413138
+(sqrt (+ 100 37))			; 11.704699917758145
+(sqrt (+ (sqrt 2) (sqrt 3)))		; 1.7739279023207892
+(square (sqrt 1000))			; 1000.000369924366
