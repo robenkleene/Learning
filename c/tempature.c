c657accf0aa81f34570ff84c4492a3d3f30536a8
Roben Kleene <contact@robenkleene.com>
2019-09-13 13:28:02 -0400

    Update program

--- c/tempature.c
+++ c/tempature.c
@@ -1,19 +1,9 @@
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table
-    for fahr = 0, 20, ..., 300; floating-point version */
+/* print Fahrenheit-Celsius table */
 main() {
-    float fahr, celsius;
-    int lower, upper, step;
+    int fahr;
 
-    lower = 0;   /* lower limit of temperature table */
-    upper = 300; /* upper limit */
-    step = 20;   /* step size */
-
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr - 32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
-    }
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+        printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
