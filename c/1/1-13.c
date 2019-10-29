4359afee97e2866d1e2eef438bc4f37413af4534
Roben Kleene <contact@robenkleene.com>
2019-10-29 15:52:38 -0400

    Improve 1-13

--- c/1/1-13.c
+++ c/1/1-13.c
@@ -9,21 +9,33 @@ int main() {
     int wcount[LENGTH] = {0};
     state = OUT;
     nc = 0;
-    while ((c = getchar()) != EOF) {
+    do {
+        c = getchar();
         if (state == IN)
             ++nc;
-        if (c == ' ' || c == '\n' || c == '\t') {
-            wcount[nc]++;
+        if (c == ' ' || c == '\n' || c == '\t' || c == EOF) {
+            if (nc > 0) {
+                if (nc < LENGTH) {
+                    wcount[nc]++;
+                } else {
+                    printf("%d is too many characters.\n", nc);
+                }
+            }
             nc = 0;
             state = OUT;
         } else if (state == OUT) {
             state = IN;
             nc = 1;
         }
-    }
+    } while (c != EOF);
     for (int i = 0; i < LENGTH; i++) {
         int cc = wcount[i];
-        if (cc > 0)
-            printf("%d %d\n", i, cc);
+        if (cc > 0) {
+            printf("%2d ", i);
+            for (int j = 0; j < cc; j++) {
+                putchar('-');
+            }
+            putchar('\n');
+        }
     }
 }
