a5b5c098a5bd444b6b715b91dad092500af50c16
Roben Kleene <contact@robenkleene.com>
2019-11-06 13:29:36 -0500

    Run formatter

--- c/1/longest_line.c
+++ c/1/longest_line.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#define MAXLINE 1000    /* maximum input line size */
+#define MAXLINE 1000 /* maximum input line size */
 
 int getline(char s[], int lim);
 void copy(char to[], char from[]);
 
 /* print longest input line */
-int main()
-{
-    int len;            /* current line length */
-    int max;            /* maximum length seen so far */
-    char line[MAXLINE];     /* current input line */
-    char longest[MAXLINE];  /* longest line saved here */
+int main() {
+    int len;               /* current line length */
+    int max;               /* maximum length seen so far */
+    char line[MAXLINE];    /* current input line */
+    char longest[MAXLINE]; /* longest line saved here */
 
     max = 0;
     while ((len = getline(line, MAXLINE)) > 0)
@@ -18,30 +17,27 @@ int main()
             max = len;
             copy(longest, line);
         }
-    if (max > 0)    /* there was a line */
+    if (max > 0) /* there was a line */
         printf("%s", longest);
     return 0;
 }
 
 /* getline:  read a line into s, return length */
-int getline(char s[], int lim)
-{
+int getline(char s[], int lim) {
     int c, i;
 
-    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
         s[i] = c;
     if (c == '\n') {
         s[i] = c;
-         ++i;
-
+        ++i;
     }
     s[i] = '\0';
     return i;
 }
 
 /* copy:  copy 'from' into 'to'; assume to is big enough */
-void copy(char to[], char from[])
-{
+void copy(char to[], char from[]) {
     int i;
 
     i = 0;
