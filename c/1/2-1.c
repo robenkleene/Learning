9ed3174da0bfb9664cfae89bb24445c566f71095
Roben Kleene <contact@robenkleene.com>
2019-11-19 13:58:26 -0500

    Work on 2-1

--- c/1/2-1.c
+++ c/1/2-1.c
@@ -3,5 +3,15 @@
 #include <float.h>
 
 int main() {
-    printf("hello, world\n");
+    printf("UCHAR_MAX = %d\n", UCHAR_MAX);
+    printf("CHAR_MAX = %d\n", CHAR_MAX);
+    printf("CHAR_MIN = %d\n", CHAR_MIN);
+    printf("SHRT_MAX = %d\n", SHRT_MAX);
+    printf("SHRT_MIN = %d\n", SHRT_MIN);
+    printf("UINT_MAX = %d\n", UINT_MAX);
+    printf("INT_MAX = %d\n", INT_MAX);
+    printf("INT_MIN = %d\n", INT_MIN);
+    printf("ULLONG_MA = %llu\n", ULLONG_MAX);
+    printf("LLONG_MAX = %lld\n", LLONG_MAX);
+    printf("LLONG_MIN = %lld\n", LLONG_MIN);
 }
