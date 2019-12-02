9327584bbde5f00b3b9b40397143bb45510c229e
Roben Kleene <contact@robenkleene.com>
2019-12-02 17:06:26 -0500

    Start 2-2

--- c/2/2-2.c
+++ c/2/2-2.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+#define MAXLINE 1000
+
 int main() {
-    for (int i, c = 0; i < lim - 1 && (c = getchar()) != '\n' && c != EOF; ++i)
+    int lim = MAXLINE;
+    char s[MAXLINE];
+    for (int i = 0, c = 0; i < lim - 1 && (c = getchar()) != '\n' && c != EOF; ++i)
         s[i] = c;
+    printf("%s", s);
 }
 
 
