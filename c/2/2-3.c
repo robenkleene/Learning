153f31680d7a82092843660a0b3c7a779898e289
Roben Kleene <contact@robenkleene.com>
2019-12-05 12:44:54 -0500

    Update 2-3

--- c/2/2-3.c
+++ c/2/2-3.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     char s[] = "0X1F";
     int v = 0X1F;
     printf("%i\n", v);
     printf("%s\n", s);
+    int result = atoi(s);
+    printf("%i\n", result);
 }
 
 int htoi(char s[]) {
