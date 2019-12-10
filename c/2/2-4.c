be865d7578d5ff55f1b61534a9bfcc79c917dd68
Roben Kleene <contact@robenkleene.com>
2019-12-10 13:27:56 -0500

    Start 2-4

---
+++ c/2/2-4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void squeeze(char s[], int c) {
+    int i, j;
+
+    for (i = j = 0; s[i] != '\0'; i++)
+        if (s[i] != c)
+            s[j++] = s[i];
+    s[j] = '\0';
+}
+
+int main() {
+    char s[] = "hello, world\n";
+    squeeze(s, 'l');
+    printf("%s", s);
+}
