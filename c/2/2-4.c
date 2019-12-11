8e0b5a0f0db910fa4ff344803189bb21fe22dc35
Roben Kleene <contact@robenkleene.com>
2019-12-11 13:36:42 -0500

    Update 2-4 function signature

--- c/2/2-4.c
+++ c/2/2-4.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-void squeeze(char s[], int c) {
+void squeeze(char s1[], char s2[]) {
     int i, j;
 
-    for (i = j = 0; s[i] != '\0'; i++)
-        if (s[i] != c)
-            s[j++] = s[i];
-    s[j] = '\0';
+    for (i = j = 0; s1[i] != '\0'; i++)
+        if (s1[i] != s2[0])
+            s1[j++] = s1[i];
+    s1[j] = '\0';
 }
 
 int main() {
     char s[] = "hello, world\n";
-    squeeze(s, 'l');
+    squeeze(s, "l");
     printf("%s", s);
 }
