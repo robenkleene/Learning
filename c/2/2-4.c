6c71f5b1e29c75ac2fdb28e6b2baa9c90cce288a
Roben Kleene <contact@robenkleene.com>
2019-12-11 13:46:14 -0500

    Finish 2-4

--- c/2/2-4.c
+++ c/2/2-4.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 void squeeze(char s1[], char s2[]) {
-    int i, j;
+    int i, j, k;
 
-    for (i = j = 0; s1[i] != '\0'; i++)
-        if (s1[i] != s2[0])
+    for (i = j = 0; s1[i] != '\0'; i++) {
+        for (k = 0; s2[k] != '\0'; k++)
+            if (s1[i] == s2[k])
+                break;
+        if (s2[k] == '\0')
             s1[j++] = s1[i];
+    }
     s1[j] = '\0';
 }
 
 int main() {
     char s[] = "hello, world\n";
-    squeeze(s, "l");
+    squeeze(s, "lew");
     printf("%s", s);
 }
