92744a904e78d3010495fdb091ae9ed9c6a5f3ae
Roben Kleene <contact@robenkleene.com>
2020-01-29 06:23:30 -0500

    Work on 2-7

--- c/2/2-7.c
+++ c/2/2-7.c
@@ -34,9 +34,12 @@ unsigned invert(unsigned x, int p, int n) {
 }
 
 int main() {
-    int x = 0b11111111111111111111;
+    int x = 0b10101010101010101010101010101010;
     int n = 5;
     int p = 9;
+    printf("n = %i, p = %i\n", n, p);
+    printf("X\n");
+    qpb(x);
     unsigned result = invert(x, p, n);
     printf("result\n");
     qpb(result);
