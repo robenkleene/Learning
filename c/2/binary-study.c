8f75d9ff3ad0e5bcc5a489cc0a2bf54b6cc1d4c4
Roben Kleene <contact@robenkleene.com>
2020-02-01 06:21:07 -0500

    Make printbits header

--- c/2/binary-study.c
+++ c/2/binary-study.c
@@ -1,22 +1,5 @@
 #include <stdio.h>
-
-void printbits(size_t const size, void const *const ptr) {
-    unsigned char *b = (unsigned char *)ptr;
-    unsigned char byte;
-    int i, j;
-
-    for (i = size - 1; i >= 0; i--) {
-        for (j = 7; j >= 0; j--) {
-            byte = (b[i] >> j) & 1;
-            printf("%u", byte);
-        }
-    }
-    puts("");
-}
-
-void qpb(int x) {
-    printbits(sizeof(x), &x);
-}
+#include "printbits.h"
 
 void test() {
     // Octal
