7e67578148f6453b99c01d020d3ec5a0edd8280d
Roben Kleene <contact@robenkleene.com>
2020-02-04 06:18:34 -0500

    Finish 2-7

--- c/2/2-7.c
+++ c/2/2-7.c
@@ -6,28 +6,11 @@ unsigned invert(unsigned x, int p, int n) {
     int offset = p + 1 - n;
 
     int left_field = ~0 << position_count;
-    printf("left field\n");
-    qpb(left_field);
-
     int right_field = ~(~0 << offset);
-    printf("right_field\n");
-    qpb(right_field);
-
     int middle_field = ~(left_field | right_field);
-    printf("middle field\n");
-    qpb(middle_field);
-
     int left = x & left_field;
-    printf("left\n");
-    qpb(left);
-
     int right = right_field & x;
-    printf("right\n");
-    qpb(right);
-
     int middle = (~(x & middle_field)) & middle_field;
-    printf("middle\n");
-    qpb(middle);
 
     return left | middle | right;
 }
