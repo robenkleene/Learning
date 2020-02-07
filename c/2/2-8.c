4221f78b2d2e872f707ae4bc3c64e70aaeb60e85
Roben Kleene <contact@robenkleene.com>
2020-02-07 06:15:31 -0500

    Update function

--- c/2/2-8.c
+++ c/2/2-8.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "printbits.h"
 
-unsigned rightrot(x, n) {
-    int shifted = x << n;
+unsigned rightrot(unsigned int x, unsigned int n) {
+    int shifted = x >> n;
     printf("shifted\n");
     qpb(shifted);
     return x;
 }
 
 int main() {
-    int x = 0b10101010101010101010101010101010;
-    int n = 5;
+    unsigned int x = 0b10101010101010101010101010101010;
+    unsigned int n = 5;
     printf("n = %i\n", n);
     printf("X\n");
     qpb(x);
