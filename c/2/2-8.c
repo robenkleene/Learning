6557c8fb4427749ddb27cabadf9c72ea091c356e
Roben Kleene <contact@robenkleene.com>
2020-02-08 06:15:44 -0500

    Use variable for unsigned

--- c/2/2-8.c
+++ c/2/2-8.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "printbits.h"
 
-unsigned rightrot(unsigned int x, unsigned int n) {
-    int shifted = x >> n;
+int rightrot(x, n) {
+    unsigned int ux = x;
+    int shifted = ux >> n;
     printf("shifted\n");
     qpb(shifted);
     return x;
 }
 
 int main() {
-    unsigned int x = 0b10101010101010101010101010101010;
-    unsigned int n = 5;
+    int x = 0b10101010101010101010101010101010;
+    int n = 5;
     printf("n = %i\n", n);
     printf("X\n");
     qpb(x);
