65f773b1617cb3e18f1abd7e3e03bc51b13899cb
Roben Kleene <contact@robenkleene.com>
2020-02-12 06:11:45 -0500

    Clean up 2-8

--- c/2/2-8.c
+++ c/2/2-8.c
@@ -4,22 +4,13 @@
 int rightrot(x, n) {
     // Right Bits
     int right_field = ~(~0 << n);
-    printf("right_field\n");
-    qpb(right_field);
     int right_bits = x & right_field;
-    printf("right_bits\n");
-    qpb(right_bits);
     int bits = sizeof(x) * 8;
-    printf("bits = %i\n", bits);
     int right_bits_shifted = right_bits << (bits - n);
-    printf("right_bits_shifted\n");
-    qpb(right_bits_shifted);
 
     // Rotate
     unsigned int ux = x;
     int shifted = ux >> n;
-    printf("shifted\n");
-    qpb(shifted);
 
     return shifted | right_bits_shifted;
 }
