e144c986acc9c463da052464a74acf1be398708e
Roben Kleene <contact@robenkleene.com>
2020-02-16 06:12:27 -0500

    Finish 2-9

--- c/2/2-9.c
+++ c/2/2-9.c
@@ -7,24 +7,14 @@
 // original `x`.
 
 int bitcount(unsigned x) {
-    int decremented = x - 1;
-    printf("decremeneted\n");
-    qpb(decremented);
-    int shifted = x;
-    shifted &= decremented;
-    printf("shifted\n");
-    qpb(shifted);
-
     int b;
-
-    for (b = 0; x != 0; x >>= 1)
-        if (x & 01)
-            b++;
+    for (b = 0; x != 0; x &= (x-1))
+        b++;
     return b;
 }
 
 int main() {
-    int x = 0b01010100000010000010000100010000;
+    int x = 0b01010100010010000010000100010000;
     printf("x\n");
     qpb(x);
     int result = bitcount(x);
