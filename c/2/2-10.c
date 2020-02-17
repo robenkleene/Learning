9c90872f3b05822a1cf4cf9294a3cea72dc9b4a0
Roben Kleene <contact@robenkleene.com>
2020-02-17 06:14:32 -0500

    Finish 2-10

--- c/2/2-10.c
+++ c/2/2-10.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 int lower(int c) {
-    if (c >= 'A' && c <= 'Z')
-        return c + 'a' - 'A';
-    else
-        return c;
+    return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
 }
 
 int main() {
