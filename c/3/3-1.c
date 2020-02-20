56ed547c6115e59ddfcdd645d28c48bb2f51004d
Roben Kleene <contact@robenkleene.com>
2020-02-20 06:16:48 -0500

    Add example 3-1

--- c/3/3-1.c
+++ c/3/3-1.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-#define LENGTH 3
+#define LENGTH 5
+
+int binsearch2(int x, int v[], int n) {
+    int low, high, mid;
+
+    low = 0;
+    high = n - 1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (x < v[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    int index = low - 1;
+    return x == v[index] ? index : -1;
+}
 
 int binsearch(int x, int v[], int n) {
     int low, high, mid;
@@ -20,8 +36,8 @@ int binsearch(int x, int v[], int n) {
 }
 
 int main() {
-    int v[LENGTH] = {1, 2, 3};
-    int x = 2;
-    int result = binsearch(x, v, LENGTH);
+    int v[LENGTH] = {11, 12, 203, 260, 600};
+    int x = 260;
+    int result = binsearch2(x, v, LENGTH);
     printf("result = %i\n", result);
 }
