9146793776c9c7e5ad9961bf2dfa9b756ff44cc1
Roben Kleene <contact@robenkleene.com>
2020-02-24 06:12:40 -0500

    Refactor in switch statement

--- c/3/3-2.c
+++ c/3/3-2.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 static void escape(char s[], char t[]) {
-    int i;
-
-    i = 0;
-    while ((t[i] = s[i]) != '\0')
+    int i = 0;
+    char c;
+    while ((c = s[i]) != '\0') {
+        switch (c) {
+            case '\n':
+            case '\t':
+                t[i] = c;
+                break;
+            default:
+                t[i] = c;
+                break;
+        }
         ++i;
+    }
 }
 
 int main() {
@@ -15,40 +24,4 @@ int main() {
     printf("start = %s\n", start);
     escape(start, result);
     printf("result = %s\n", result);
-
-    /* int c, i, nwhite, nother, ndigit[10]; */
-
-    /* nwhite = nother = 0; */
-    /* for (i = 0; i < 10; i++) */
-    /*     ndigit[i] = 0; */
-    /* while ((c = getchar()) != EOF) { */
-    /*     switch (c) { */
-    /*         case '0': */
-    /*         case '1': */
-    /*         case '2': */
-    /*         case '3': */
-    /*         case '4': */
-    /*         case '5': */
-    /*         case '6': */
-    /*         case '7': */
-    /*         case '8': */
-    /*         case '9': */
-    /*             ndigit[c -'0']++; */
-    /*             break; */
-    /*         case ' ': */
-    /*         case '\n': */
-    /*         case '\t': */
-    /*             nwhite++; */
-    /*             break; */
-    /*         default: */
-    /*             nother++; */
-    /*             break; */
-    /*     } */
-    /* } */
-
-    /* printf("digits ="); */
-    /* for (i = 0; i < 10; i++) */
-    /*     printf(" %d", ndigit[i]); */
-    /* printf(", white space = %d, other = %d\n", nwhite, nother); */
-    /* return 0; */
 }
