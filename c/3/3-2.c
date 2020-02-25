9ce5c685fda256fa2e9fdfc80118fe09d5fd48ee
Roben Kleene <contact@robenkleene.com>
2020-02-25 06:14:02 -0500

    Finish escape

--- c/3/3-2.c
+++ c/3/3-2.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 
+#define MAXLENGTH 1000
+
 static void escape(char s[], char t[]) {
-    int i = 0;
+    int i = 0, j = 0;
     char c;
     while ((c = s[i]) != '\0') {
         switch (c) {
             case '\n':
+                t[j] = '\\';
+                j++;
+                t[j] = 'n';
+                break;
             case '\t':
-                t[i] = c;
+                t[j] = '\\';
+                j++;
+                t[j] = 't';
                 break;
             default:
-                t[i] = c;
+                t[j] = c;
                 break;
         }
         ++i;
+        ++j;
     }
 }
 
 int main() {
     char start[] = "A string with a \t and a \n";
-    size_t size = sizeof(start) / sizeof(char);
-    char result[size];
-    printf("start = %s\n", start);
+    char result[MAXLENGTH];
+    printf("Initial\n%s\n", start);
     escape(start, result);
-    printf("result = %s\n", result);
+    printf("Escaped\n%s\n", result);
 }
