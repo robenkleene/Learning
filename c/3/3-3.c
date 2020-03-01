e0fcdbbecd66b59aada8e4c35822c5fa6088c93b
Roben Kleene <contact@robenkleene.com>
2020-03-01 08:01:42 -0500

    Setup function

--- c/3/3-3.c
+++ c/3/3-3.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 void expand(char s1[], char s2[]) {
     char c;
-    for (int i = 0, j = 0; i < strlen(s1); i++, j++) {
+    int length = strlen(s1);
+    for (int i = 0, j = 0; i < length; i++, j++) {
         c = s1[i];
-        if (s1[i+1] == '-') {
+        if (i + 2 < length) {
+            char c2 = s1[i + 2];
+            if ((isalpha(c) || isdigit(c)) &&
+            s1[i + 1] == '-' &&
+            (isalpha(c2) || isdigit(c2))) {
+            } else {
+                putchar(c);
+            }
+        } else {
+            putchar(c);
         }
-        putchar(c);
     }
 }
 
@@ -16,7 +26,7 @@ int main() {
         "notations like a-z in the string s1 into the equivalent complete list"
         "abc...xyz in s2. Allow for letters of either case and digits, and be"
         "prepared to handle cases like a-b-c and a-z0−9 and -a-z. Arrange that"
-        "a leading or trailing - is taken literally-.";
+        "a leading or trailing - is taken literally-.a-";
     char s2[0];
     expand(s1, s2);
     printf("s1 = %s\n", s1);
