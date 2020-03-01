e76b1c32e296cc3a3c5c1a7b5b3566235cc99e39
Roben Kleene <contact@robenkleene.com>
2020-03-01 08:02:03 -0500

    Run formatting

--- c/3/3-3.c
+++ c/3/3-3.c
@@ -1,6 +1,6 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
 void expand(char s1[], char s2[]) {
     char c;
@@ -9,9 +9,7 @@ void expand(char s1[], char s2[]) {
         c = s1[i];
         if (i + 2 < length) {
             char c2 = s1[i + 2];
-            if ((isalpha(c) || isdigit(c)) &&
-            s1[i + 1] == '-' &&
-            (isalpha(c2) || isdigit(c2))) {
+            if ((isalpha(c) || isdigit(c)) && s1[i + 1] == '-' && (isalpha(c2) || isdigit(c2))) {
             } else {
                 putchar(c);
             }
@@ -23,10 +21,10 @@ void expand(char s1[], char s2[]) {
 
 int main() {
     char s1[] = "Write a function expand(s1,s2) that expands shorthand"
-        "notations like a-z in the string s1 into the equivalent complete list"
-        "abc...xyz in s2. Allow for letters of either case and digits, and be"
-        "prepared to handle cases like a-b-c and a-z0−9 and -a-z. Arrange that"
-        "a leading or trailing - is taken literally-.a-";
+                "notations like a-z in the string s1 into the equivalent complete list"
+                "abc...xyz in s2. Allow for letters of either case and digits, and be"
+                "prepared to handle cases like a-b-c and a-z0−9 and -a-z. Arrange that"
+                "a leading or trailing - is taken literally-.a-";
     char s2[0];
     expand(s1, s2);
     printf("s1 = %s\n", s1);
