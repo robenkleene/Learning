6f5bcfeb076881fa9b3da03fad9f3b6e88ab2269
Roben Kleene <contact@robenkleene.com>
2020-03-03 06:25:42 -0500

    Work on 3-3

--- c/3/3-3.c
+++ c/3/3-3.c
@@ -11,14 +11,18 @@ int issupported(char c1, char c2) {
 void expand(char s1[], char s2[]) {
     char c1;
     int length = strlen(s1);
-    for (int i = 0, j = 0; i < length; i++, j++) {
+    for (int i = 0; i < length; i++) {
         c1 = s1[i];
         if (i + 2 < length) {
             char c2 = s1[i + 2];
-            if (s1[i + 1] == '-' && issupported(c1, c2)) {
-                /* i += 2; */
-
-                putchar(c1);
+            if (s1[i + 1] == '-' &&
+                issupported(c1, c2) &&
+                c1 < c2) {
+                i += 2;
+                for (int j = c1; j <= c2; j++) {
+                    // TODO: Replace s2, instead of `putchar`
+                    putchar(j);
+                }
             } else {
                 putchar(c1);
             }
@@ -29,12 +33,12 @@ void expand(char s1[], char s2[]) {
 }
 
 int main() {
-    char s1[] = "Write a function expand(s1,s2) that expands shorthand"
-                "notations like a-z in the string s1 into the equivalent complete list"
-                "abc...xyz in s2. Allow for letters of either case and digits, and be"
-                "prepared to handle cases like a-b-c and a-z0−9 and -a-z. Arrange that"
-                "a leading or trailing - is taken literally-.a-";
+    char s1[] = "Write a function expand(s1,s2) that expands z-a shorthand"
+                " notations like a-z in the string s1 into the equivalent complete list"
+                " abc...xyz in s2. Allow for letters of either case and digits, and be"
+                " prepared to handle cases like a-b-c and a-z0−9 and -a-z. Arrange that"
+                " a leading or trailing - is taken literally-.a-";
     char s2[0];
+    printf("s1 = %s\n\n", s1);
     expand(s1, s2);
-    printf("s1 = %s\n", s1);
 }
