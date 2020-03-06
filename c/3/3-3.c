9354c1cbd5804857fcc67b6f466a40b6d78cc6de
Roben Kleene <contact@robenkleene.com>
2020-03-06 06:13:03 -0500

    Fix input string

--- c/3/3-3.c
+++ c/3/3-3.c
@@ -23,7 +23,6 @@ void expand(char s1[], char s2[]) {
                 c1 < c2) {
                 i += 2;
                 for (int k = c1; k <= c2; k++, j++) {
-                    /* printf("j in = %i\n", j); */
                     s2[j] = k;
                 }
                 // `j` will be incremented at the star of the `for` loop
@@ -42,7 +41,7 @@ int main() {
     char s1[] = "Write a function expand(s1, s2) that expands z-a shorthand"
                 " notations like a-z in the string s1 into the equivalent complete list"
                 " abc...xyz in s2. Allow for letters of either case and digits, and be"
-                " prepared to handle cases like a-b-c and a-z0−9 and -a-z. Arrange that"
+                " prepared to handle cases like a-b-c and a-z0-9 and -a-z. Arrange that"
                 " a leading or trailing - is taken literally-.a-";
     char s2[LENGTH];
     printf("s1 = %s\n\n", s1);
