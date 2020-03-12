a86cd4fba8a3ea05aaf6c2cc2c15812075ec338c
Roben Kleene <contact@robenkleene.com>
2020-03-12 06:15:25 -0400

    Expand debug code

--- c/3/3-4.c
+++ c/3/3-4.c
@@ -24,10 +24,12 @@ void itoa(int n, char s[]) {
         /* record sign */
         /* make n positive */
         n = -n;
+    printf("n = %i\n", n);
     i = 0;
     do {
         /* generate digits in reverse order */
         /* get next digit */
+        printf("n mod 10 = %i\n", n % 10);
         s[i++] = n % 10 + '0';
 
         printf("i = %i\n", i);
