de7d3c187707a08e40ed749f74793ee4cc36a782
Roben Kleene <contact@robenkleene.com>
2020-08-12 19:40:33 -0400

    Add print list function

--- tutorial/linkedlist.c
+++ tutorial/linkedlist.c
@@ -3,18 +3,27 @@
 
 typedef struct node {
     int val;
-    struct node * next;
+    struct node *next;
 } node_t;
 
+void print_list(node_t *head) {
+    node_t *current = head;
+
+    while (current != NULL) {
+        printf("%d\n", current->val);
+        current = current->next;
+    }
+}
+
 int main() {
-    node_t * head = NULL;
+    node_t *head = NULL;
     head = (node_t *)malloc(sizeof(node_t));
     if (head == NULL) {
         return 1;
     }
 
     head->val = 1;
-    head->next = (node_t *) malloc(sizeof(node_t));
+    head->next = (node_t *)malloc(sizeof(node_t));
     head->next->val = 2;
     head->next->next = NULL;
 }
