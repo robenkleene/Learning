330723bfd0589daad905d410b64088c467ba8354
Roben Kleene <contact@robenkleene.com>
2020-08-13 14:00:11 -0400

    Add remaining functions

--- tutorial/linkedlist.c
+++ tutorial/linkedlist.c
@@ -15,6 +15,86 @@ void print_list(node_t *head) {
     }
 }
 
+void append(node_t *head, int val) {
+    node_t *current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    current->next = (node_t *)malloc(sizeof(node_t));
+    current->next->val = val;
+    current->next->next = NULL;
+}
+
+void push(node_t **head, int val) {
+    node_t *new_node;
+    new_node = (node_t *)malloc(sizeof(node_t));
+
+    new_node->val = val;
+    new_node->next = *head;
+    *head = new_node;
+}
+
+int pop(node_t **head) {
+    int retval = -1;
+    node_t *next_node = NULL;
+
+    if (*head == NULL) {
+        return -1;
+    }
+
+    next_node = (*head)->next;
+    retval = (*head)->val;
+    free(*head);
+    *head = next_node;
+
+    return retval;
+}
+
+int remove_last(node_t *head) {
+    int retval = 0;
+    if (head->next == NULL) {
+        retval = head->val;
+        free(head);
+        return retval;
+    }
+
+    node_t *current = head;
+    while (current->next->next != NULL) {
+        current = current->next;
+    }
+
+    retval = current->next->val;
+    free(current->next);
+    current->next = NULL;
+    return retval;
+}
+
+int remove_by_index(node_t **head, int n) {
+    int i = 0;
+    int retval = -1;
+    node_t *current = *head;
+    node_t *temp_node = NULL;
+
+    if (n == 0) {
+        return pop(head);
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        if (current->next == NULL) {
+            return -1;
+        }
+        current = current->next;
+    }
+
+    temp_node = current->next;
+    retval = temp_node->val;
+    current->next = temp_node->next;
+    free(temp_node);
+
+    return retval;
+}
+
 int main() {
     node_t *head = NULL;
     head = (node_t *)malloc(sizeof(node_t));
