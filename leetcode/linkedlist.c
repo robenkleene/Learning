a15669839b505a528339b966cdd3b7aaf6710f97
Roben Kleene <contact@robenkleene.com>
2020-08-28 08:00:02 -0400

    Add WIP print code

--- leetcode/linkedlist.c
+++ leetcode/linkedlist.c
@@ -2,21 +2,30 @@
 
 typedef struct {
     int val;
-    struct node *next;
+    struct MyLinkedList *next;
 } MyLinkedList;
 
-/** Initialize your data structure here. */
+void printList(MyLinkedList *obj) {
+    MyLinkedList *current = obj;
+    while (current != NULL) {
+        printf("%d\n", current->val);
+        current = current->next;
+    }
+}
 
+/** Initialize your data structure here. */
 MyLinkedList* myLinkedListCreate() {
     return NULL;
 }
 
-/** Get the value of the index-th node in the linked list. If the index is invalid, return -1. */
+/** Get the value of the index-th node in the linked list. If the index is
+ * invalid, return -1. */
 int myLinkedListGet(MyLinkedList* obj, int index) {
     return 0;
 }
 
-/** Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list. */
+/** Add a node of value val before the first element of the linked list. After
+ * the insertion, the new node will be the first node of the linked list. */
 void myLinkedListAddAtHead(MyLinkedList* obj, int val) {
   
 }
@@ -26,7 +35,10 @@ void myLinkedListAddAtTail(MyLinkedList* obj, int val) {
   
 }
 
-/** Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted. */
+/** Add a node of value val before the index-th node in the linked list. If
+ * index equals to the length of linked list, the node will be appended to the
+ * end of linked list. If index is greater than the length, the node will not
+ * be inserted. */
 void myLinkedListAddAtIndex(MyLinkedList* obj, int index, int val) {
   
 }
