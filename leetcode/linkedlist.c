6e10b205c97ef7a97812697cfc4c52e9110bbab3
Roben Kleene <contact@robenkleene.com>
2020-09-09 07:32:09 -0400

    Work on insert

--- leetcode/linkedlist.c
+++ leetcode/linkedlist.c
@@ -18,10 +18,10 @@ void printList(MyLinkedList *obj) {
 
 /** Initialize your data structure here. */
 MyLinkedList *myLinkedListCreate() {
-    MyLinkedList *node = (MyLinkedList *)malloc(sizeof(MyLinkedList));
-    node->val = 0;
-    node->next = NULL;
-    return node;
+    MyLinkedList *new = (MyLinkedList *)malloc(sizeof(MyLinkedList));
+    new->val = 0;
+    new->next = NULL;
+    return new;
 }
 
 /** Get the value of the index-th node in the linked list. If the index is
@@ -41,10 +41,10 @@ int myLinkedListGet(MyLinkedList *obj, int index) {
 /** Add a node of value val before the first element of the linked list. After
  * the insertion, the new node will be the first node of the linked list. */
 void myLinkedListAddAtHead(MyLinkedList **obj, int val) {
-    MyLinkedList *node = (MyLinkedList *)malloc(sizeof(MyLinkedList));
-    node->val = val;
-    node->next = *obj;
-    *obj = node;
+    MyLinkedList *new = (MyLinkedList *)malloc(sizeof(MyLinkedList));
+    new->val = val;
+    new->next = *obj;
+    *obj = new;
 }
 
 /** Append a node of value val to the last element of the linked list. */
@@ -59,10 +59,10 @@ void myLinkedListAddAtTail(MyLinkedList **obj, int val) {
         current = current->next;
     }
 
-    MyLinkedList *node = (MyLinkedList *)malloc(sizeof(MyLinkedList));
-    node->val = val;
-    node->next = NULL;
-    current->next = node;
+    MyLinkedList *new = (MyLinkedList *)malloc(sizeof(MyLinkedList));
+    new->val = val;
+    new->next = NULL;
+    current->next = new;
 }
 
 /** Add a node of value val before the index-th node in the linked list. If
@@ -72,8 +72,14 @@ void myLinkedListAddAtTail(MyLinkedList **obj, int val) {
 void myLinkedListAddAtIndex(MyLinkedList *obj, int index, int val) {
     MyLinkedList *current = obj;
     for (int i = 0; i < index; i++) {
-        // TODO: Continue from here
+        if (current->next == NULL) {
+            return;
+        }
+        current = current->next;
     }    
+
+    MyLinkedList *next = current->next;
+    MyLinkedList *new = (MyLinkedList *)malloc(sizeof(MyLinkedList));
 }
 
 /** Delete the index-th node in the linked list, if the index is valid. */
