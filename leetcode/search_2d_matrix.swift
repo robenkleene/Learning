c9b8b1392fc3e959cc71f44f2cbc11f9b2e093c2
Roben Kleene <contact@robenkleene.com>
2020-12-04 07:27:33 -0500

    Fix bug

--- leetcode/search_2d_matrix.swift
+++ leetcode/search_2d_matrix.swift
@@ -2,7 +2,7 @@ class Solution {
   func search(_ matrix: [[Int]], _ target: Int) -> (Int, Int)? {
     for i in 0..<matrix.count {
       let submatrix = matrix[i]
-      guard i < matrix.count else {
+      guard submatrix.count > 0 else {
           return nil
       }
       if target >= submatrix[0] {
@@ -25,10 +25,10 @@ class Solution {
 
 // let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]]
 // let target = 20
-// var matrix = [[Int]]()
-// matrix.append([Int]())
-// let target = 1
-var matrix = [[1],[3]]
-let target = 3
+var matrix = [[Int]]()
+matrix.append([Int]())
+let target = 1
+// var matrix = [[1],[3]]
+// let target = 3
 let result = Solution().search(matrix, target)
 print(result ?? "Not found")
