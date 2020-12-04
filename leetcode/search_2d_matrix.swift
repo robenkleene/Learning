bc6d97dbef74159f682a8fa6e387010336e6d97c
Roben Kleene <contact@robenkleene.com>
2020-12-04 07:10:12 -0500

    Update example

--- leetcode/search_2d_matrix.swift
+++ leetcode/search_2d_matrix.swift
@@ -20,7 +20,10 @@ class Solution {
   }
 }
 
-let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]]
-let target = 20
+// let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]]
+// let target = 20
+var matrix = [[Int]]()
+matrix.append([Int]())
+let target = 1
 let result = Solution().search(matrix, target)
 print(result ?? "Not found")
