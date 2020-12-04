3402e6f42a24e9a2ead162863929f496b82f9591
Roben Kleene <contact@robenkleene.com>
2020-12-04 06:59:33 -0500

    Search matrix

---
+++ leetcode/search_2d_matrix.swift
@@ -0,0 +1,26 @@
+class Solution {  
+  func search(_ matrix: [[Int]], _ target: Int) -> (Int, Int)? {
+    for i in 0..<matrix.count {
+      let submatrix = matrix[i]
+      if target > submatrix[0] {
+        for j in 0..<submatrix.count {
+          if submatrix[j] == target {
+            return (i, j)
+          }
+        }
+      }
+    }
+
+    return nil
+  }
+
+  
+  func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+    return search(matrix, target) != nil
+  }
+}
+
+let matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]]
+let target = 20
+let result = Solution().search(matrix, target)
+print(result ?? "Not found")
