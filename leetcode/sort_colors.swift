cb3a054395867a858623f0d84e4fb2ae9a671855
Roben Kleene <contact@robenkleene.com>
2020-12-13 06:11:34 -0500

    Work on sort colors

---
+++ leetcode/sort_colors.swift
@@ -0,0 +1,14 @@
+#!/usr/bin/swift
+
+import Foundation
+
+class Solution {
+    func sortColors(_ nums: inout [Int]) {
+        
+    }
+}
+
+var nums = [2,0,2,1,1,0]
+print(nums)
+Solution().sortColors(&nums)
+print(nums)
