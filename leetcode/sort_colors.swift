52bed09d361e9d76123a825f3bbf2c2580d637cd
Roben Kleene <contact@robenkleene.com>
2020-12-13 06:33:55 -0500

    Add swift version

--- leetcode/sort_colors.swift
+++ leetcode/sort_colors.swift
@@ -4,11 +4,25 @@ import Foundation
 
 class Solution {
     func sortColors(_ nums: inout [Int]) {
-        
+        var curr = 0
+        var p0 = 0
+        var p2 = nums.count - 1
+        while curr <= p2 {
+            if nums[curr] == 0 {
+                (nums[p0], nums[curr]) = (nums[curr], nums[p0])
+                p0 += 1
+                curr += 1
+            } else if nums[curr] == 2 {
+                (nums[curr], nums[p2]) = (nums[p2], nums[curr])
+                p2 -= 1
+            } else {
+                curr += 1
+            }
+        }
     }
 }
 
-var nums = [2,0,2,1,1,0]
+var nums = [2, 0, 2, 1, 1, 0]
 print(nums)
 Solution().sortColors(&nums)
 print(nums)
