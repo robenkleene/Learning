aef8487e967b2667fe81c7bfec2dd5345a93d2d0
Roben Kleene <contact@robenkleene.com>
2020-12-13 07:06:52 -0500

    Switch to switch

--- leetcode/sort_colors.swift
+++ leetcode/sort_colors.swift
@@ -8,17 +8,18 @@ class Solution {
         var p0 = 0
         var p2 = nums.count - 1
         while curr <= p2 {
-            if nums[curr] == 0 {
-                if p0 != curr {
-                    (nums[p0], nums[curr]) = (nums[curr], nums[p0])
-                }
-                p0 += 1
-                curr += 1
-            } else if nums[curr] == 2 {
-                (nums[curr], nums[p2]) = (nums[p2], nums[curr])
-                p2 -= 1
-            } else {
-                curr += 1
+            switch nums[curr] {
+                case 0:
+                    if p0 != curr {
+                        (nums[p0], nums[curr]) = (nums[curr], nums[p0])
+                    }
+                    p0 += 1
+                    curr += 1
+                case 2:
+                    (nums[curr], nums[p2]) = (nums[p2], nums[curr])
+                    p2 -= 1
+                default:
+                    curr += 1
             }
         }
     }
