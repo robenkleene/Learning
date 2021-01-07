d38345af8d51e386750e048ae9440a78772a0a73
Roben Kleene <contact@robenkleene.com>
2021-01-07 06:53:21 -0500

    Fix recursive

--- tutorial/tree_traversal.swift
+++ tutorial/tree_traversal.swift
@@ -118,10 +118,10 @@ func dfsRecu<T: Equatable>(start: Node<T>, goal: T? = nil, visited: [Node<T>]? =
         return visited
     }
     if let left = start.left {
-        visited += dfsRecu(start: left, visited: visited)
+        visited += dfsRecu(start: left, visited: [left])
     }
     if let right = start.right {
-        visited += dfsRecu(start: right, visited: visited)
+        visited += dfsRecu(start: right, visited: [right])
     }
 
     return visited
