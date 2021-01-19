73a689bb12ffeacd4972cde86811b3cdc7e0ec34
Roben Kleene <contact@robenkleene.com>
2021-01-19 08:20:17 -0500

    Add sort

---
+++ tutorial/sort.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Insertion Sort
+def insertion(arr):
+    for i in range(1, len(arr)):
+        save = arr[i]
+        j = i
+        while j > 0 and arr[j - 1] > save:
+            arr[j] = arr[j - 1]
+            j -= 1
+        arr[j] = save
+
+# Quick Sort
+def partition(array, begin, end):
+    pivot = begin
+    for i in range(begin+1, end+1):
+        if array[i] <= array[begin]:
+            pivot += 1
+            array[i], array[pivot] = array[pivot], array[i]
+    array[pivot], array[begin] = array[begin], array[pivot]
+    return pivot
+
+def quicksort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+    def _quicksort(array, begin, end):
+        if begin >= end:
+            return
+        pivot = partition(array, begin, end)
+        _quicksort(array, begin, pivot-1)
+        _quicksort(array, pivot+1, end)
+    return _quicksort(array, begin, end)
+
+# Selection Sort
+def selection(arr):
+    for i in range(len(arr)):
+        mini = min(arr[i:])
+        min_index = arr[i:].index(mini)
+        arr[i + min_index] = arr[i]
+        arr[i] = mini
+    return arr
+
+arr = [1, 2, 1, 5, 3, 2, 5]
+print("arr =", arr)
+result = selection(arr)
+print("result =", result)
