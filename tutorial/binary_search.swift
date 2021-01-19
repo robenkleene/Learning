1467486683e9df32843cc24473c605df2bf4e7b4
Roben Kleene <contact@robenkleene.com>
2021-01-18 20:06:58 -0500

    Write binary search

--- tutorial/binary_search.swift
+++ tutorial/binary_search.swift
@@ -1,5 +1,19 @@
 #!/usr/bin/swift
 
 func bs_iter<T: Comparable>(arr: [T], target: T) -> Int? {
+    var start = 0, end = arr.count - 1
+    while start <= end {
+        let mid = start + end / 2
+        let val = arr[mid]
+        if target < val {
+            end = mid - 1
+        } else if target > val {
+            start = mid + 1
+        } else {
+            return mid
+        }
+    }
     return nil
 }
+
+
