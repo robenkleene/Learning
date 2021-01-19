9faf87b3785518d76a6fd090e03f187b180146ff
Roben Kleene <contact@robenkleene.com>
2021-01-18 20:38:55 -0500

    Finish binary search recursive

--- tutorial/binary_search.swift
+++ tutorial/binary_search.swift
@@ -16,11 +16,28 @@ func bs_iter<T: Comparable>(_ arr: [T], _ target: T) -> Int? {
     return nil
 }
 
+
+func bs_recu<T: Comparable>(_ arr: [T], _ target: T, start: Int = 0, end: Int = arr.count - 1) -> Int? {
+    if start > end {
+        return nil
+    }
+
+    let mid = (start + end) / 2
+    let val = arr[mid]
+    if target < val {
+        return bs_recu(arr, target, start: start, end: mid - 1)
+    } else if target > val {
+        return bs_recu(arr, target, start: mid + 1, end: end)
+    } else {
+        return mid
+    }
+}
+
 let target = 4
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 print("arr =", arr)
-let result = bs_iter(arr, 4)
+var result = bs_iter(arr, 4)
 print("result = \(result?.description ?? "")")
-// result = bs_recu(arr, 4)
-// print("result =", result)
+result = bs_recu(arr, 4)
+print("result = \(result?.description ?? "")")
 
