5ba742cc56e8865dfe69808bfcfb667c2200b1f5
Roben Kleene <contact@robenkleene.com>
2021-01-20 15:50:04 -0500

    Add merge sort

--- tutorial/sort.py
+++ tutorial/sort.py
@@ -44,6 +44,27 @@ def selection(arr):
         arr[i] = minimum
     return arr
 
+# Merge Sort
+def merge_sort(arr):
+    if len(arr) < 2:
+        return arr
+    result = []
+    mid = int(len(arr) / 2)
+    y = merge_sort(arr[:mid])
+    z = merge_sort(arr[mid:])
+    i = 0
+    j = 0
+    while i < len(y) and j < len(z):
+        if y[i] > z[j]:
+            result.append(z[j])
+            j += 1
+        else:
+            result.append(y[i])
+            i += 1
+    result += y[i:]
+    result += z[j:]
+    return result
+
 arr = [1, 2, 1, 5, 3, 2, 5]
 print("arr =", arr)
 
