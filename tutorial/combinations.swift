d139b430bb59b85036d8e3261ba6f3b8b10a2c18
Roben Kleene <contact@robenkleene.com>
2021-01-21 09:34:42 -0500

    Setup combinations

---
+++ tutorial/combinations.swift
@@ -0,0 +1,22 @@
+#!/usr/bin/swift
+
+func combinations(arr1: [Any], arr2: [Any]) -> [Any] {
+    var result = [Any]
+    for item1 in arr1 {
+        for item2 in arr2 {
+
+        }
+    }
+    return result
+}
+
+func permutations(arr1: [Any], arr2: [Any]) -> [Any]{
+    var result = [Any]
+
+    return result
+}
+
+let arr1 = [1, 2, 3, 4]
+let arr2 = ["a", "b"]
+
+
