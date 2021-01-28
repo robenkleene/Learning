d1830ec730c4e6ba95a607b4a93c8a556a8b39a5
Roben Kleene <contact@robenkleene.com>
2021-01-28 07:34:09 -0500

    Setup node

--- tutorial/lru.swift
+++ tutorial/lru.swift
@@ -1,3 +1,12 @@
 #!/usr/bin/swift
 
-
+class Node<T: Hashable> {
+    var value: T
+    var key: String
+    var next: Node?
+    var prev: Node?
+    init(key: String, value: T) {
+        self.key = key
+        self.value = value
+    }
+}
