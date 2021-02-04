2acea2fad5b64c08a0f04f0464df8ec91e432b5a
Roben Kleene <contact@robenkleene.com>
2021-02-04 15:45:50 -0500

    Rename variable

--- tutorial/spiral.py
+++ tutorial/spiral.py
@@ -4,12 +4,12 @@ def spiral(matrix):
     if not matrix: return []
     R, C = len(matrix), len(matrix[0])
     seen = [[False] * C for _ in matrix]
-    ans = []
+    result = []
     dr = [0, 1, 0, -1]
     dc = [1, 0, -1, 0]
     r = c = di = 0
     for _ in range(R * C):
-        ans.append(matrix[r][c])
+        result.append(matrix[r][c])
         seen[r][c] = True
         cr, cc = r + dr[di], c + dc[di]
         if 0 <= cr < R and 0 <= cc < C and not seen[cr][cc]:
@@ -17,7 +17,7 @@ def spiral(matrix):
         else:
             di = (di + 1) % 4
             r, c = r + dr[di], c + dc[di]
-    return ans
+    return result
 
 input = [[1,2,3],[4,5,6],[7,8,9]]
 result = spiral(input)
