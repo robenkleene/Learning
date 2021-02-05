0f2119bf05bed044680f6938d6d287021e876b28
Roben Kleene <contact@robenkleene.com>
2021-02-04 20:36:28 -0500

    Rename constant

--- tutorial/spiral.py
+++ tutorial/spiral.py
@@ -5,18 +5,18 @@ def spiral(matrix):
     ROWS, COLS = len(matrix), len(matrix[0])
     visited = [[False] * COLS for _ in matrix]
     result = []
-    dr = [0, 1, 0, -1]
-    dc = [1, 0, -1, 0]
+    DR = [0, 1, 0, -1]
+    DC = [1, 0, -1, 0]
     r = c = dir = 0
     for _ in range(ROWS * COLS):
         result.append(matrix[r][c])
         visited[r][c] = True
-        cr, cc = r + dr[dir], c + dc[dir]
+        cr, cc = r + DR[dir], c + DC[dir]
         if 0 <= cr < ROWS and 0 <= cc < COLS and not visited[cr][cc]:
             r, c = cr, cc
         else:
             dir = (dir + 1) % 4
-            r, c = r + dr[dir], c + dc[dir]
+            r, c = r + DR[dir], c + DC[dir]
     return result
 
 input = [[1,2,3],[4,5,6],[7,8,9]]
