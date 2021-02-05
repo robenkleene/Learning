09318b80cfdf7e254b2e9a6b78fca2ff4470ce6f
Roben Kleene <contact@robenkleene.com>
2021-02-04 21:01:46 -0500

    Clean up

--- tutorial/spiral.py
+++ tutorial/spiral.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 def spiral(matrix):
-    if not matrix: return []
     ROWS, COLS = len(matrix), len(matrix[0])
     visited = [[False] * COLS for _ in matrix]
     result = []
@@ -11,10 +10,14 @@ def spiral(matrix):
     for _ in range(ROWS * COLS):
         result.append(matrix[r][c])
         visited[r][c] = True
-        cr, cc = r + DR[dir], c + DC[dir]
+        # Create a potential move
+        cr, cc = r + DR[dir], c + DC[dir] # Move in dir
+        # Check if an edge, or visited cell, has been hit
         if 0 <= cr < ROWS and 0 <= cc < COLS and not visited[cr][cc]:
+            # Continue moving in that dir
             r, c = cr, cc
         else:
+            # Or turn
             dir = (dir + 1) % 4
             r, c = r + DR[dir], c + DC[dir]
     return result
