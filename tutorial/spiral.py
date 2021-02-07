39cd695d7bf49ae92b7c6336ce8fe6a54e3e1e29
Roben Kleene <contact@robenkleene.com>
2021-02-07 13:31:07 -0500

    Use tuple

--- tutorial/spiral.py
+++ tutorial/spiral.py
@@ -3,23 +3,21 @@
 def spiral(matrix):
     ROWS, COLS = len(matrix), len(matrix[0])
     visited = [[False] * COLS for _ in matrix]
-    DR = [0, 1, 0, -1]
-    DC = [1, 0, -1, 0]
+    DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     r = c = dir = 0
     result = []
     for _ in range(ROWS * COLS):
         result.append(matrix[r][c])
         visited[r][c] = True
-        # Create a potential move
-        cr, cc = r + DR[dir], c + DC[dir] # Move in dir
-        # Check if an edge, or visited cell, has been hit
-        if 0 <= cr < ROWS and 0 <= cc < COLS and not visited[cr][cc]:
-            # Continue moving in that dir
-            r, c = cr, cc
+        offs_r, offs_c = DIRS[dir]
+        next_r, next_c = r + offs_r, c + offs_c
+        if 0 <= next_r < ROWS and 0 <= next_c < COLS and not visited[next_r][next_c]:
+            r, c = next_r, next_c
         else:
-            # Or turn
             dir = (dir + 1) % 4
-            r, c = r + DR[dir], c + DC[dir]
+            offs_r, offs_c = DIRS[dir]
+            next_r, next_c = r + offs_r, c + offs_c
+            r, c = next_r, next_c
     return result
 
 input = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
