925609a609379c51b6efb59f6216f2004da1de83
Roben Kleene <contact@robenkleene.com>
2021-02-07 13:38:05 -0500

    Setup variables

--- tutorial/spiral.swift
+++ tutorial/spiral.swift
@@ -4,5 +4,7 @@ func spiral(_ matrix: [[Int]]) -> [Int] {
     let (rows, cols) = (matrix.count, matrix[0].count)
     let falseRow = Array(repeating: false, count: cols)
     var visited = [Array(repeating: [falseRow], count: rows)]
+    let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    var (r, c, dir) = (0, 0, 0)
     return [Int]()
 }
