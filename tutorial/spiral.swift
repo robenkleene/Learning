57541c89b91041ff8ec02fa096415a443b8da75d
Roben Kleene <contact@robenkleene.com>
2021-02-07 13:08:16 -0500

    Setup false

--- tutorial/spiral.swift
+++ tutorial/spiral.swift
@@ -2,5 +2,7 @@
 
 func spiral(_ matrix: [[Int]]) -> [Int] {
     let (rows, cols) = (matrix.count, matrix[0].count)
+    let falseRow = Array(repeating: false, count: cols)
+    var visited = [Array(repeating: [falseRow], count: rows)]
     return [Int]()
 }
