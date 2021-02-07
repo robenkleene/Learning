62b50e145e849fa13354d78944f5084b7d60e20e
Roben Kleene <contact@robenkleene.com>
2021-02-07 13:59:52 -0500

    Work on Swift

--- tutorial/spiral.swift
+++ tutorial/spiral.swift
@@ -3,8 +3,14 @@
 func spiral(_ matrix: [[Int]]) -> [Int] {
     let (rows, cols) = (matrix.count, matrix[0].count)
     let falseRow = Array(repeating: false, count: cols)
-    var visited = [Array(repeating: [falseRow], count: rows)]
+    var visited = Array(repeating: falseRow, count: rows)
     let directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     var (r, c, dir) = (0, 0, 0)
+    var result = [Int]()
+    for _ in 0..<(rows * cols) {
+        result.append(matrix[r][c])
+        visited[r][c] = true
+        let (offsR, offsC) = directions[dir]
+    }
     return [Int]()
 }
