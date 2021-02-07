deae363d1155e442faae394e2219fac46dc24a93
Roben Kleene <contact@robenkleene.com>
2021-02-07 15:48:19 -0500

    Work on Swift LRU

--- tutorial/lru.swift
+++ tutorial/lru.swift
@@ -8,14 +8,51 @@ class Node<Key: Hashable, Value> {
 }
 
 struct LRUCache<Key: Hashable, Value> {
-    var cache = [Key: Value]()
+    typealias NodeType = Node<Key, Value>
+    var cache = [Key: NodeType]()
     var size = 0
     let capacity: Int
-    let head = Node<Key, Value>()
-    let tail = Node<Key, Value>()
+    let head = NodeType()
+    let tail = NodeType()
     init(capacity: Int) {
         self.capacity = capacity
         head.next = tail
-        tail.next = head
+        tail.prev = head
+    }
+
+    private func add(_ node: NodeType) {
+        node.prev = head
+        node.next = head.next
+        head.next?.prev = node
+        head.next = node
+    }
+
+    private func remove(_ node: NodeType) {
+        guard let prev = node.prev, let next = node.next else {
+            return
+        }
+        prev.next = next
+        next.prev = prev
+    }
+
+    private func moveToHead(_ node: NodeType) {
+        remove(node)
+        add(node)
+    }
+
+    private func pop() -> NodeType? {
+        guard let node = tail.prev, node !== head else {
+            return nil
+        }
+        remove(node)
+        return node
+    }
+
+    func value(for key: Key) -> Value? {
+        guard let node = cache[key] else {
+            return nil
+        }
+        moveToHead(node)
+        return node.value
     }
 }
