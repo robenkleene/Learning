23b2f789e48204ccb19635c8c35c4e14dbe0ac31
Roben Kleene <contact@robenkleene.com>
2021-02-09 11:52:47 -0500

    Add depth

--- tutorial/diameter.py
+++ tutorial/diameter.py
@@ -77,6 +77,16 @@ def diameter(root):
     _, ans = depth(root)
     return ans - 1
 
+def max_depth(root):
+    """
+    max_depth
+    """
+    if root is None:
+        return 0
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+    return max(left_depth, right_depth) + 1
+
 def main():
     """
     main
