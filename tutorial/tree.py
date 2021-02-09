8e72432791220dcd0fa15ce2fbb32b0457c3253d
Roben Kleene <contact@robenkleene.com>
2021-02-09 12:55:51 -0500

    Refactor and rename

--- tutorial/tree.py
+++ tutorial/tree.py
@@ -62,29 +62,74 @@ class Tree:
         tree.root = root
         return tree
 
+def dfs(start, goal=None):
+    """
+    dfs_iter
+    """
+    visited, stack = [], [start]
+    while stack:
+        curr = stack.pop()
+        visited.append(curr)
+        if goal is not None and curr.val == goal:
+            return visited
+        if curr.left is not None:
+            stack.append(curr.left)
+        if curr.right is not None:
+            stack.append(curr.right)
+    return visited
+
+def bfs(start, goal=None):
+    """
+    bfs_iter
+    """
+    visited, queue = [], [start]
+    while queue:
+        curr = queue.pop(0)
+        visited.append(curr)
+        if goal is not None and curr.val == goal:
+            return visited
+        if curr.left is not None:
+            queue.append(curr.left)
+        if curr.right is not None:
+            queue.append(curr.right)
+    return visited
+
+def dfs_recu(curr, goal=None, visited=None):
+    """
+    dfs_recu
+    """
+    visited = visited or [curr]
+    if goal is not None and curr.val == goal:
+        return visited
+    if curr.left is not None:
+        visited += dfs_recu(curr.left, goal, [curr.left])
+    if curr.right is not None:
+        visited += dfs_recu(curr.right, goal, [curr.right])
+    return visited
+
 def diameter(root):
     """
     diameter
     """
-    def depth(node, result = 1):
+    def depth_diameter(node, result = 1):
         if not node:
             return 0, result
-        depth_left, result = depth(node.left, result)
-        depth_right, result = depth(node.right, result)
+        depth_left, result = depth_diameter(node.left, result)
+        depth_right, result = depth_diameter(node.right, result)
         result = max(result, depth_left + depth_right + 1)
         return max(depth_left, depth_right) + 1, result
 
-    _, result = depth(root)
+    _, result = depth_diameter(root)
     return result - 1
 
-def max_depth(node):
+def depth(node):
     """
-    max_depth
+    depth
     """
     if node is None:
         return 0
-    left_depth = max_depth(node.left)
-    right_depth = max_depth(node.right)
+    left_depth = depth(node.left)
+    right_depth = depth(node.right)
     return max(left_depth, right_depth) + 1
 
 def main():
@@ -95,7 +140,7 @@ def main():
     print(tree)
     result = diameter(tree.root)
     print("diameter =", result)
-    result = max_depth(tree.root)
-    print("max_depth =", result)
+    result = depth(tree.root)
+    print("depth =", result)
 
 main()
