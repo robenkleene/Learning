ad21e006a84350ea402e97b18070b2c0b6feb216
Roben Kleene <contact@robenkleene.com>
2021-02-09 15:21:49 -0500

    Switch order

--- tutorial/binary_tree.py
+++ tutorial/binary_tree.py
@@ -76,10 +76,10 @@ def insert(node, val):
     if node is None:
         return Node(val)
 
-    if val > node.val:
-        node.right = insert(node.right, val)
-    else:
+    if val < node.val:
         node.left = insert(node.left, val)
+    else:
+        node.right = insert(node.right, val)
     return node
 
 def search(node, val):
