d9dacc4eb5dcdda6715f749196e54ea4118b6929
Roben Kleene <contact@robenkleene.com>
2021-03-05 07:27:26 -0500

    Clean up

--- leetcode/4sum.swift
+++
@@ -1,9 +0,0 @@
-class Solution {
-    func fourSum(_ nums: [Int], _ target: Int) -> [[Int]] {
-        return [[Int]]()
-    }
-}
-
-let nums = [1,0,-1,0,-2,2]
-let target = 0
-let result = Solution().fourSum(nums, 0)
