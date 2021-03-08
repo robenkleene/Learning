b2b2ae20e6f550ec52437bae38c4feaa8f27f44d
Roben Kleene <contact@robenkleene.com>
2021-03-08 07:32:58 -0500

    Fix linter errors

--- tutorial/spiral.py
+++ tutorial/spiral.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 
+"""
+spiral
+"""
+
 def spiral(matrix):
+    """
+    spiral
+    """
     ROWS, COLS = len(matrix), len(matrix[0])
     visited = [[False] * COLS for _ in matrix]
     DIRS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    r = c = dir = 0
+    r = c = direct = 0
     result = []
     for _ in range(ROWS * COLS):
         result.append(matrix[r][c])
         visited[r][c] = True
-        offs_r, offs_c = DIRS[dir]
+        offs_r, offs_c = DIRS[direct]
         next_r, next_c = r + offs_r, c + offs_c
         if 0 <= next_r < ROWS and 0 <= next_c < COLS and not visited[next_r][next_c]:
             r, c = next_r, next_c
         else:
-            dir = (dir + 1) % 4
-            offs_r, offs_c = DIRS[dir]
+            direct = (direct + 1) % 4
+            offs_r, offs_c = DIRS[direct]
             next_r, next_c = r + offs_r, c + offs_c
             r, c = next_r, next_c
     return result
@@ -24,8 +31,8 @@ def main():
     """
     main
     """
-    input = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    result = spiral(input)
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    result = spiral(matrix)
     print("result =", result)
 
 main()
