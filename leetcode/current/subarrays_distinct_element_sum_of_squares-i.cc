a6468f50630da2757af2249a81e4afdde62fe86e
Roben Kleene <contact@robenkleene.com>
2024-09-19 13:58:50 +0000

    Start writing loop

--- leetcode/current/subarrays_distinct_element_sum_of_squares-i.cc
+++ leetcode/current/subarrays_distinct_element_sum_of_squares-i.cc
@@ -4,12 +4,15 @@
 using namespace std;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
 }
 
 class Solution {
-public:
-    int sumCounts(vector<int>& nums) {
+  public:
+    int sumCounts(vector<int> &nums) {
+        for (std::size_t i = 0; i < arr.size(); ++i) {
+            std::cout << arr[i] << std::endl;
+        }
         return 0;
     }
 };
