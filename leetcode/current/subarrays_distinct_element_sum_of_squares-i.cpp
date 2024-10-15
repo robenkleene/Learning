eee8da450713821542cc4fa62e0b8bdb214131d3
Roben Kleene <contact@robenkleene.com>
2024-10-15 10:57:39 +0000

    Work on subarray

--- leetcode/current/subarrays_distinct_element_sum_of_squares-i.cpp
+++ leetcode/current/subarrays_distinct_element_sum_of_squares-i.cpp
@@ -6,15 +6,26 @@ using namespace std;
 class Solution {
   public:
     int sumCounts(vector<int> &nums) {
+      // Debug code
+      std::cout << "nums = ";
+      for (std::size_t i = 0; i < nums.size(); ++i) {
+          std::cout << (i > 0 ? ", " : "") << nums[i];
+      }
+      std::cout << std::endl;
+
       // Iterate through all subarrays
       for (std::size_t i = 0; i < nums.size(); ++i) {
         for (std::size_t j = i; j < nums.size(); ++j) {
+          // Create an array with the elements at indexes from `i` to `j`
           vector<int> subarray(nums.begin() + i, nums.begin() + j + 1);
+
+          // Debug code
           std::cout << "subarray = ";
           for (std::size_t i = 0; i < subarray.size(); ++i) {
               std::cout << (i > 0 ? ", " : "") << subarray[i];
           }
           std::cout << std::endl;
+
         }
       }
       return 0;
