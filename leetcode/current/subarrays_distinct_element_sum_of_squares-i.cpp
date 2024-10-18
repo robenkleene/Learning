94eae75bc3e4ae77146156950cc0d6de020ea9e1
Roben Kleene <contact@robenkleene.com>
2024-10-18 10:47:11 +0000

    Return the square

--- leetcode/current/subarrays_distinct_element_sum_of_squares-i.cpp
+++ leetcode/current/subarrays_distinct_element_sum_of_squares-i.cpp
@@ -1,6 +1,7 @@
+#include <cmath>
 #include <iostream>
-#include <vector>
 #include <unordered_set>
+#include <vector>
 
 using namespace std;
 
@@ -12,28 +13,17 @@ class Solution {
       for (std::size_t i = 0; i < nums.size(); ++i) {
         for (std::size_t j = i; j < nums.size(); ++j) {
           std::unordered_set<int> subset(nums.begin() + i, nums.begin() + j + 1);
-          sum += subset.size();
-
-          // Debug code
-          std::cout << "subset = ";
-          for (auto it = subset.begin(); it != subset.end(); ++it) {
-            if (it != subset.begin()) {
-              std::cout << ", ";
-            }
-            std::cout << *it;
-          }
-          std::cout << std::endl;
-          //
-
+          sum += std::pow(subset.size(), 2);
         }
       }
-      return 0;
+      return sum;
     }
 };
 
 int main() {
   vector<int> nums = {1, 2, 1};
   Solution solution;
-  solution.sumCounts(nums);
+  int result = solution.sumCounts(nums);
+  std::cout << "result = " << result << std::endl;
 }
 
