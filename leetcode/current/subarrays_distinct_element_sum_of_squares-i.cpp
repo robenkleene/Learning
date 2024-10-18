e92ebe1b83cb4c39c24400ca8af809ddbfb9e4a2
Roben Kleene <contact@robenkleene.com>
2024-10-18 10:41:30 +0000

    Sum the sets

--- leetcode/current/subarrays_distinct_element_sum_of_squares-i.cpp
+++ leetcode/current/subarrays_distinct_element_sum_of_squares-i.cpp
@@ -7,19 +7,12 @@ using namespace std;
 class Solution {
   public:
     int sumCounts(vector<int> &nums) {
-      // Debug code
-      std::cout << "nums = ";
-      for (std::size_t i = 0; i < nums.size(); ++i) {
-          std::cout << (i > 0 ? ", " : "") << nums[i];
-      }
-      std::cout << std::endl;
-      //
+      int sum = 0;
 
-      // Iterate through all subarrays
       for (std::size_t i = 0; i < nums.size(); ++i) {
         for (std::size_t j = i; j < nums.size(); ++j) {
-          // Create an array with the elements at indexes from `i` to `j`
           std::unordered_set<int> subset(nums.begin() + i, nums.begin() + j + 1);
+          sum += subset.size();
 
           // Debug code
           std::cout << "subset = ";
