18db5805df0a11f9481f098152c1278f530c0a65
Roben Kleene <contact@robenkleene.com>
2024-10-21 10:42:24 +0000

    Add unordered map

--- leetcode/current/two_sums_optimized.cpp
+++ leetcode/current/two_sums_optimized.cpp
@@ -1,10 +1,12 @@
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
   public:
     vector<int> twoSum(vector<int>& nums, int target) {
+      unordered_map<int, bool> values;
       for (int i = 0; i < nums.size(); i++) {
         for (int j = i + 1; j < nums.size(); j++) {
           int sum = nums[i] + nums[j];
