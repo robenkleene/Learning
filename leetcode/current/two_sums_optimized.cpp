6243350c66fcf7b062e56f5659b951b175ddb78e
Roben Kleene <contact@robenkleene.com>
2024-10-28 11:00:42 +0000

    Make solution one pass

--- leetcode/current/two_sums_optimized.cpp
+++ leetcode/current/two_sums_optimized.cpp
@@ -9,27 +9,12 @@ class Solution {
     vector<int> twoSum(vector<int>& nums, int target) {
       unordered_map<int, int> values;
       for (int i = 0; i < nums.size(); i++) {
-        values[nums[i]] = i;
-      }
-
-      // Debug
-      std::cout << this << " values = {";
-      bool first = true;
-      for (const auto& pair : values) {
-        if (!first) {
-          std::cout << ", ";
-        }
-        std::cout << "{" << pair.first << ": " << pair.second << "}";
-        first = false;
-      }
-      std::cout << "}" << std::endl;
-
-      for (int i = 0; i < nums.size(); i++) {
         int value = nums[i];
         int diff = target - value;
-        if (values.count(diff) > 0 &&  i != values[diff]) {
-          return vector<int>{i, values[diff]};
+        if (values.count(diff) > 0) {
+          return vector<int>{values[diff], i};
         }
+        values[value] = i;
       }
       return vector<int>{};
     }
