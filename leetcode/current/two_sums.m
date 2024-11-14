2f4b1a18e414172ea05bb94d72c18e2b00695fdd
Roben Kleene <contact@robenkleene.com>
2024-11-14 06:49:54 -0500

    Update Objective-C

---
+++ leetcode/current/two_sums.m
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@interface Solution : NSObject
+ - (NSInteger)twoSum(nums: NSInteger, target: NSInteger);
+@end
+NS_ASSUME_NONNULL_END
+
+@implementation
+- (NSInteger)twoSum(nums: NSInteger, target: NSInteger) {
+    return 0;
+}
+@end}
+
+int main() {
+    NSLog(@"Hello, World!");
+}
