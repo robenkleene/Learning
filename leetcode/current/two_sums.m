50d32f5df493dbf94598647356be6a98356bdecf
Roben Kleene <contact@robenkleene.com>
2024-11-19 06:59:16 -0500

    Work on two sums

--- leetcode/current/two_sums.m
+++ leetcode/current/two_sums.m
@@ -2,14 +2,17 @@
 
 NS_ASSUME_NONNULL_BEGIN
 @interface Solution : NSObject
-+ (NSArray<NSNumber *> *)twoSumWithNums:(NSInteger)nums target:(NSInteger)target;
++ (NSArray<NSNumber *> *)twoSumWithNums:(NSArray<NSNumber *> *)nums target:(NSInteger)target;
 @end
 NS_ASSUME_NONNULL_END
 
 @implementation Solution
-+ (NSArray<NSNumber *> *)twoSumWithNums:(NSInteger)nums target:(NSInteger)target {
++ (NSArray<NSNumber *> *)twoSumWithNums:(NSArray<NSNumber *> *)nums target:(NSInteger)target {
 
     NSMutableDictionary<NSNumber *, NSNumber *> *values = [NSMutableDictionary dictionary];
+    for (int i = 0; i < [nums count]; i++) {
+        NSLog(@"%d", i);
+    }
     return @[];
 }
 @end
