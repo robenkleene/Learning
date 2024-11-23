bc6eea2791a14b9cb4890897cbf8fc1859eefde5
Roben Kleene <contact@robenkleene.com>
2024-11-23 07:02:01 -0500

    Update problem

--- leetcode/current/cpp/add_two_numbers.cpp
+++ leetcode/current/cpp/add_two_numbers.cpp
@@ -1,3 +1,8 @@
+/*
+   Input: l1 = [2,4,3], l2 = [5,6,4]
+   Output: [7,0,8]
+   Explanation: 342 + 465 = 807.
+*/
 struct ListNode {
     int val;
     ListNode *next;
@@ -9,6 +14,12 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-
+      ListNode *c1 = l1;
+      ListNode *result;
+      while (c1 != nullptr) {
+        int v1 = l1->val;
+        c1 = l1->next;
+      }
+      return result;
     }
 };
