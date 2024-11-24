deb7c004a624f267bc3bd6ec2fb84fedec88e859
Roben Kleene <contact@robenkleene.com>
2024-11-24 06:54:51 -0500

    Work on adding numbers

--- leetcode/current/cpp/add_two_numbers.cpp
+++ leetcode/current/cpp/add_two_numbers.cpp
@@ -4,27 +4,31 @@
    Explanation: 342 + 465 = 807.
 */
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-      ListNode *c1 = l1;
-      ListNode *c2 = l2;
-      ListNode *result;
-      while (c1 != nullptr) {
-        int v1 = l1->val;
-        while (c2 != nullptr) {
-          int v2 = l2->val;
-          c2 = l2->next;
-        }
-        c1 = l1->next;
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode *c1 = l1;
+    ListNode *c2 = l2;
+    ListNode *result = new ListNode();
+    ListNode *c = result;
+    while (c1 != nullptr) {
+      int v1 = l1->val;
+      while (c2 != nullptr) {
+        int v2 = l2->val;
+        c2 = l2->next;
+        c->val = v1 + v2;
+        c->next = new ListNode();
+        c = c->next;
       }
-      return result;
+      c1 = l1->next;
     }
+    return result;
+  }
 };
