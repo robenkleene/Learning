a49a42cd8ab732959bda26027d6b6f22f1077f76
Roben Kleene <contact@robenkleene.com>
2024-11-29 18:45:21 -0500

    Finish setting up test cases

--- leetcode/current/cpp/add_two_numbers.cpp
+++ leetcode/current/cpp/add_two_numbers.cpp
@@ -13,6 +13,28 @@ struct ListNode {
   ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+class Solution {
+public:
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode *c1 = l1;
+    ListNode *c2 = l2;
+    ListNode *result = new ListNode();
+    ListNode *c = result;
+    while (c1 != nullptr) {
+      int v1 = l1->val;
+      while (c2 != nullptr) {
+        int v2 = l2->val;
+        c2 = l2->next;
+        c->val = v1 + v2;
+        c->next = new ListNode();
+        c = c->next;
+      }
+      c1 = l1->next;
+    }
+    return result;
+  }
+};
+
 void printList(ListNode* head) {
   ListNode* current = head;
   while (current != nullptr) {
@@ -41,34 +63,18 @@ ListNode* arrayToLinkedList(const std::vector<int>& arr) {
 void testCase(const std::vector<int>& a1, const std::vector<int>& a2) {
   ListNode* l1 = arrayToLinkedList(a1);
   ListNode* l2 = arrayToLinkedList(a2);
+  std::cout << "l1: ";
   printList(l1);
+  std::cout << "l2: ";
   printList(l2);
+  Solution *solution = new Solution();
+  ListNode* result = solution->addTwoNumbers(l1, l2);
+  std::cout << "re: ";
+  printList(result);
+  std::cout << "\n";
 }
 
-class Solution {
-public:
-  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-    ListNode *c1 = l1;
-    ListNode *c2 = l2;
-    ListNode *result = new ListNode();
-    ListNode *c = result;
-    while (c1 != nullptr) {
-      int v1 = l1->val;
-      while (c2 != nullptr) {
-        int v2 = l2->val;
-        c2 = l2->next;
-        c->val = v1 + v2;
-        c->next = new ListNode();
-        c = c->next;
-      }
-      c1 = l1->next;
-    }
-    return result;
-  }
-};
-
 int main() {
-  Solution *solution = new Solution();
   testCase({2, 4, 3}, {5,6,4});
   testCase({0}, {0});
   testCase({9, 9, 9, 9, 9, 9, 9}, {9, 9, 9, 9});
